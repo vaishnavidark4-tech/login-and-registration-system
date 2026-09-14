58b8c49699f63a9b4e66496a279bd9be8e91fd0b
Vaishnavi Darkunde <vaishnavidark4@gmail.com>
2026-09-14 12:02:29 +0530

    Update SecurityConfig.java

--- src/main/java/com/vaishnavi/loginapp/config/SecurityConfig.java
+++ src/main/java/com/vaishnavi/loginapp/config/SecurityConfig.java
@@ -142,7 +142,8 @@ public class SecurityConfig {
                         "http://127.0.0.1:5173",
 
                         "http://localhost:5174",
-                        "http://127.0.0.1:5174"
+                        "http://127.0.0.1:5174",
+                    "https://vaishnavidark4-tech.github.io"
                 )
         );
 
